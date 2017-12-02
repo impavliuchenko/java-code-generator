46031354fd9f097cb4939d0637b8b0b653fc006d
Ivan Pavliuchenko <impavlyuchenko@gmail.com>
2017-12-03 00:18:31 +0200

    Initial commit

---
+++ backend/src/main/java/com/codegen/JavaCodeGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.codegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCodeGeneratorApplication.class, args);
+	}
+}
