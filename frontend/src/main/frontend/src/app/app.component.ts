386776ef6d400f7f47e09090fcf2c1399160aad4
Ivan Pavliuchenko <impavlyuchenko@gmail.com>
2017-12-25 00:16:46 +0200

    frontend: add desinged authentication form

--- frontend/src/main/frontend/src/app/app.component.ts
+++ frontend/src/main/frontend/src/app/app.component.ts
@@ -4,7 +4,7 @@ import {CarService} from './app.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent{
   constructor (private carService: CarService){ }
